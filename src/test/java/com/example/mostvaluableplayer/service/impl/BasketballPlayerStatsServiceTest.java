2a1ffa4af00b5e879687a67addc98258a2ab9da1
vadim <vadimoselskyi@gmail.com>
2023-02-17 00:53:50 +0200

    -Add test classes

---
+++ src/test/java/com/example/mostvaluableplayer/service/impl/BasketballPlayerStatsServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.mostvaluableplayer.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasketballPlayerStatsServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getPlayerStats() {
+    }
+}
---
+++ src/test/java/com/example/mostvaluableplayer/service/impl/BasketballServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.mostvaluableplayer.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasketballServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void calculateRating() {
+    }
+
+    @Test
+    void defineWinners() {
+    }
+
+    @Test
+    void calculateGameRating() {
+    }
+}
