805454ccded52d1ea73575354a5b0eb742b2f84b
Oselskyi <vadimoselskyi@gmail.com>
2023-02-23 16:33:36 +0200

    -Update commit renamed enum Sport to SportType

--- src/main/java/com/example/mostvaluableplayer/model/SportType.java
+++ src/main/java/com/example/mostvaluableplayer/model/SportType.java
@@ -7,10 +7,10 @@ import lombok.ToString;
 @AllArgsConstructor
 @Getter
 @ToString
-public enum Sport {
+public enum SportType {
     BASKETBALL("BASKETBALL"), HANDBALL("HANDBALL");
 
-    private String name;
+    private final String name;
 
     public String getName() {
         return name;
