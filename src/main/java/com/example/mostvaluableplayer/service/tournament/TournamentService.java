de9852919b57c02583ec619dbb3e3afe115181ce
Oselskyi <vadimoselskyi@gmail.com>
2023-02-23 16:40:24 +0200

    -Update commit. Finished implementing TournamentService

--- src/main/java/com/example/mostvaluableplayer/service/tournament/TournamentService.java
+++ src/main/java/com/example/mostvaluableplayer/service/tournament/TournamentService.java
@@ -1,5 +1,8 @@
 package com.example.mostvaluableplayer.service.tournament;
 
+import com.example.mostvaluableplayer.model.Sportsman;
+
 public interface TournamentService {
+    Sportsman playTournament(String filesPath);
 
 }
